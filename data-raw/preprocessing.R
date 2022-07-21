d4bb8813c1028f7d002b28c9b63620feffc2ae90
laurabiggins <laurabiggins10@gmail.com>
2022-07-21 13:11:40 +0100

    Added R6 class VCall

--- data-raw/preprocessing.R
+++ data-raw/preprocessing.R
@@ -1,31 +1,120 @@
 library(tidyverse)
+library(R6)
 
-ds1 <- readr::read_tsv("data-raw/ydall.txt")
+#ds1 <- readr::read_tsv("data-raw/ydall.txt")
 ds2 <- readr::read_tsv("data-raw/umtboth.txt")
+my_ds <- parsing_wrapper(ds2, "umtboth")
+my_ds$get_Jcalls("IGHV1-12")
+
+VCall <- R6Class("VCall", list(
+  
+  name = NULL,
+  J_calls = NA,
+  D_calls = NA,
+  np_lengths = NA,
+  aa_lengths = NA,
+  
+  initialize = function(name, J_calls=NA, D_calls=NA, np_lengths = NA, aa_lengths = NA) {
+    stopifnot(is.character(name), length(name) == 1)
+
+    self$name <- name
+    self$J_calls <- J_calls
+    self$D_calls <- D_calls
+    self$np_lengths <- np_lengths
+    self$aa_lengths <- aa_lengths
+  },
+  
+  get_Jcalls = function(v_call) {
+    if(v_call %in% self$J_calls$V_CALL) {
+      dplyr::filter(self$J_calls, V_CALL==v_call)
+    }
+  },
+  
+  get_Dcalls = function(v_call) {
+    if(v_call %in% self$D_calls$V_CALL) {
+      dplyr::filter(self$D_calls, V_CALL==v_call)
+    }
+  },
+  
+  get_np_lengths= function(v_call) {
+    if(v_call %in% self$np_lengths$V_CALL) {
+      dplyr::filter(self$np_lengths, V_CALL==v_call)
+    }
+  }
+  
+))
 
 
-aa_lengths1 <- ds1 %>%
-  #select(V_CALL, CDR3_IGBLAST_AA) %>%
-  select(CDR3_IGBLAST_AA) %>%
-  rename(AA = CDR3_IGBLAST_AA) %>%
-  mutate(n_aa = nchar(AA)) %>%
-  count(n_aa)
+process_J_calls <- function(dataset){
+  dplyr::count(dataset, V_CALL, J_CALL)
+}
 
-aa_lengths2 <- ds2 %>%
-  #select(V_CALL, CDR3_IGBLAST_AA) %>%
-  select(CDR3_IGBLAST_AA) %>%
-  rename(AA = CDR3_IGBLAST_AA) %>%
-  mutate(n_aa = nchar(AA)) %>%
-  count(n_aa)
+process_D_calls <- function(dataset){
+  dataset |>
+    dplyr::select(V_CALL, D_CALL) |>
+    tidyr::drop_na(D_CALL) |>
+    tidyr::separate(D_CALL, into=c("singleD"), sep = ",", extra = "drop") |>
+    dplyr::count(V_CALL, singleD)
+}
+
+process_np_lengths <- function(dataset){
+  dplyr::select(dataset, V_CALL, NP1_LENGTH, NP2_LENGTH)
+}
 
 # tibble that contains a column called CDR3_IGBLAST_AA (or similar) that contains all the aa sequences
-summary_aa_lengths <- function(dataset, dataset_name, cdr3_col = "CDR3_IGBLAST_AA"){
-  dataset %>%
-    select({cdr3_col}) %>%
-    rename(AA = {cdr3_col}) %>%
-    mutate(n_aa = nchar(AA)) %>%
-    count(n_aa, name = dataset_name)
+process_aa_lengths <- function(dataset, cdr3_col = "CDR3_IGBLAST_AA"){
+  dataset |>
+    dplyr::select({cdr3_col}) |>
+    dplyr::rename(AA = {cdr3_col}) |>
+    dplyr::mutate(n_aa = nchar(AA)) |>
+    dplyr::count(n_aa)
 }
+
+
+parsing_wrapper <- function(dataset, dataset_name){
+  
+  J <- process_J_calls(dataset)
+  D <- process_D_calls(dataset)
+  np <- process_np_lengths(dataset)
+  aa <- process_aa_lengths(dataset)
+  VCall$new(dataset_name, J_calls=J, D_calls=D, np_lengths = np, aa_lengths = aa)
+}
+
+
+
+
+
+
+
+
+
+
+my_J_calls <- ds2 |>
+  count(V_CALL, J_CALL)
+
+D_calls <- ds2 |>
+  select(V_CALL, D_CALL) |>
+  drop_na(D_CALL) |>
+  separate(D_CALL, into=c("singleD"), sep = ",", extra = "drop") |>
+  count(V_CALL, singleD)
+
+np_lengths <- ds2 |>
+  select(V_CALL, NP1_LENGTH, NP2_LENGTH)
+
+
+
+library(ggplot2)
+J_calls %>%
+  filter(V_CALL == "IGHV1-11") %>%
+  ggplot(aes(x = J_CALL, y =n)) +
+  geom_col()
+
+
+
+aa_lengths1 <- summary_aa_lengths(ds1, "ydall")
+aa_lengths2 <- process_aa_lengths(ds2)
+
+aa_lengths <- full_join(aa_lengths1, aa_lengths2)
 
 summary1 <- ds1 %>%
   select(V_CALL, CDR3_IGBLAST_AA) %>%
@@ -150,7 +239,9 @@ joined %>%
   plotly::add_heatmap() #+
 #scale_color_manual(values = my_colours)
 
-
+joined %>%
+  dplyr::filter(V_CALL == "IGHV10-1") %>%
+  filter(pos ==1)
 
 
 # aa_counts1 <- summary1 %>%
