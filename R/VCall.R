3ccaae6e762fd291328c2f6a8030f4409ee182de
Laura Biggins <bigginsl@babraham.ac.uk>
2022-08-08 12:06:09 +0100

    Started documenting R6 class.

--- R/VCall.R
+++ R/VCall.R
@@ -1,58 +1,68 @@
-library(R6)
+#' R6 Class Representing a Person
+#'
+#' @description
+#' A VCall contains a set of Vcalls and various other data fields.
+#'
+#' @details
+#' Some more info here.
 
-VCall <- R6Class("VCall", list(
+VCall <- R6::R6Class("VCall", 
+  public = list(
   
-  name = NULL,
-  J_calls = NA,
-  D_calls = NA,
-  V_calls = NA,
-  np_lengths = NA,
-  aa_lengths = NA,
-  aa_counts_left = NA,
-  
-  initialize = function(
-    name, 
-    J_calls=NA, 
-    D_calls=NA, 
+    #' @field name Name of the dataset.
+    name = NULL,
+    
+    #' @field J_calls The J calls.
+    J_calls = NA,
+    D_calls = NA,
     V_calls = NA,
-    np_lengths = NA, 
+    np_lengths = NA,
     aa_lengths = NA,
-    aa_counts_left = NA
-  ) {
+    aa_counts_left = NA,
     
-    stopifnot(is.character(name), length(name) == 1)
+    initialize = function(
+      name, 
+      J_calls=NA, 
+      D_calls=NA, 
+      V_calls = NA,
+      np_lengths = NA, 
+      aa_lengths = NA,
+      aa_counts_left = NA
+    ) {
+      
+      stopifnot(is.character(name), length(name) == 1)
+      
+      self$name <- name
+      self$J_calls <- J_calls
+      self$D_calls <- D_calls
+      self$V_calls = V_calls
+      self$np_lengths <- np_lengths
+      self$aa_lengths <- aa_lengths
+      self$aa_counts_left <- aa_counts_left
+    },
     
-    self$name <- name
-    self$J_calls <- J_calls
-    self$D_calls <- D_calls
-    self$V_calls = V_calls
-    self$np_lengths <- np_lengths
-    self$aa_lengths <- aa_lengths
-    self$aa_counts_left <- aa_counts_left
-  },
-  
-  get_Jcalls = function(v_call) {
-    if(v_call %in% self$J_calls$V_CALL) {
-      dplyr::filter(self$J_calls, V_CALL==v_call)
+    get_Jcalls = function(v_call) {
+      if(v_call %in% self$J_calls$V_CALL) {
+        dplyr::filter(self$J_calls, V_CALL==v_call)
+      }
+    },
+    
+    get_Dcalls = function(v_call) {
+      if(v_call %in% self$D_calls$V_CALL) {
+        dplyr::filter(self$D_calls, V_CALL==v_call)
+      }
+    },
+    
+    get_np_lengths= function(v_call) {
+      if(v_call %in% self$np_lengths$V_CALL) {
+        dplyr::filter(self$np_lengths, V_CALL==v_call)
+      }
+    },
+    
+    get_aa_counts_left = function(v_call) {
+      if(v_call %in% self$aa_counts_left$V_CALL) {
+        dplyr::filter(self$aa_counts_left, V_CALL==v_call)
+      }
     }
-  },
-  
-  get_Dcalls = function(v_call) {
-    if(v_call %in% self$D_calls$V_CALL) {
-      dplyr::filter(self$D_calls, V_CALL==v_call)
-    }
-  },
-  
-  get_np_lengths= function(v_call) {
-    if(v_call %in% self$np_lengths$V_CALL) {
-      dplyr::filter(self$np_lengths, V_CALL==v_call)
-    }
-  },
-  
-  get_aa_counts_left = function(v_call) {
-    if(v_call %in% self$aa_counts_left$V_CALL) {
-      dplyr::filter(self$aa_counts_left, V_CALL==v_call)
-    }
-  }
   
 ))
