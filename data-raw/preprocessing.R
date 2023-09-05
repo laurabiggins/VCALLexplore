5f6a2f46226282cd0fc73ca16604f04fdec1f764
laurabiggins <laurabiggins10@gmail.com>
2023-09-05 16:11:25 +0100

    Added new datasets

--- data-raw/preprocessing.R
+++ data-raw/preprocessing.R
@@ -3,8 +3,15 @@ library(R6)
 source("R/processing_utils.R")
 source("R/VCall.R")
 
-# my_ds <- parsing_wrapper(read_delim("D:/temp/Harry_White/BCcurated.csv"), "BC")
-# saveRDS(my_ds, file = "data/BC.rds")
+file_names <- list.files("D:/temp/Harry_White/")
+ds_names <- gsub(file_names, pattern = ".csv", replacement = "", fixed = TRUE)
+
+for(i in 1:10){
+  file_name <- file_names[i]
+  ds_name <- ds_names[i]
+  my_ds <- parsing_wrapper(read_delim(paste0("D:/temp/Harry_White/",file_name)), ds_name)
+  saveRDS(my_ds, file = paste0("data/", ds_name, ".rds"))
+}
 # 
 # my_ds <- parsing_wrapper(read_delim("D:/temp/Harry_White/T1curated.csv"), "T1")
 # saveRDS(my_ds, file = "data/T1.rds")
